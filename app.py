87a1485bb095946304b6f66aa876e0686355cd38
seiichi okada <okas65482@gmail.com>
2022-02-17 14:42:03 +0900

    初期データ

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template ,request ,redirect ,session
+import sqlite3, random
+
+app = Flask(__name__)
+
+app.secret_key = "akibaco"
+
+
+@app.route("/" , methods = ["get"])
+def login_get():
+    return render_template("top.html")
+
+@app.route("/" , methods = ["post"])
+def login_post():
+    name = request.form.get("user_name")
+    password = request.form.get("password")
+    conn = sqlite3.connect("akibacoDB.db")
+    c = conn.cursor()
+    c.execute("SELECT id FROM users where name = ? and password = ?", (name,password))
+    id = c.fetchone()
+    c.close()
+    if id is None:
+        return render_template("top.html")
+    else:
+        session["id"]=id[0]
+        print(id)
+        return redirect("/map")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run (debug=True)
