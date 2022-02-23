2fe543082713caa34e109c0d0e4c331bc2d766b4
seiichi okada <okas65482@gmail.com>
2022-02-24 02:24:10 +0900

    0224

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ def login_get():
     return render_template("top.html")
     
 
-# @app.route("/" , methods = ["post"])
-# def login_post():
-#     name = request.form.get("user_name")
-#     password = request.form.get("password")
-#     conn = sqlite3.connect("akibacoDB.db")
-#     c = conn.cursor()
-#     c.execute("SELECT id FROM users where name = ? and password = ?", (name,password))
-#     id = c.fetchone()
-#     c.close()
-#     if id is None:
-#         return render_template("top.html")
-#     else:
-#         session["id"]=id[0]
-#         print(id)
-#         return redirect("/map")
+@app.route("/" , methods = ["post"])
+def login_post():
+    name = request.form.get("user_name")
+    password = request.form.get("password")
+    conn = sqlite3.connect("akibacoDB.db")
+    c = conn.cursor()
+    c.execute("SELECT id FROM users where name = ? and password = ?", (name,password))
+    id = c.fetchone()
+    c.close()
+    if id is None:
+        return render_template("top.html")
+    else:
+        session["id"]=id[0]
+        print(id)
+        return redirect("/map")
 
 # 投稿でっせ
 @app.route("/map", methods =["GET"])
