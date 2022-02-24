89e0b1d7e164fd8e8f6d1b4ae5c2745826b1d5f4
seiichi okada <okas65482@gmail.com>
2022-02-24 14:01:38 +0900

    24.1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template ,request ,redirect ,session
 import sqlite3
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.secret_key = "akibaco"
 
 
@@ -28,13 +28,12 @@ def login_post():
         return redirect("/map")
 
 
-
 # 投稿でっせ
-@app.route("/list", methods =["GET"])
+@app.route("/map", methods =["GET"])
 def add_get():
     return render_template("map.html")
 
-@app.route("/list", methods = ["POST"])
+@app.route("/map", methods = ["POST"])
 def add_post():
     task = request.form.get("task_kist")
     conn = sqlite3.connect("akibacoDB.db")
@@ -42,21 +41,30 @@ def add_post():
     c.execute("Insert into post_column values (null,?,?)",(task,user_id))
     conn.commit()
     c.close()
-    return redirect("/list")
+    return redirect("/map")
+
 
 # マップ情報でっせ
 
 #@app.route("")
 
-# @app.route("")
-
-
-
 
 # 投稿リストでっせ
-
-
-
+@app.route("/map")
+def list():
+    if "user_id" in session:
+        user_id = session["user_id"]
+        conn = sqlite3.connect("akibacoDB.db")
+        c = conn.cursor()
+        c.execute("SELECT id, task FROM task where user_id = ?", (user_id,))
+        task_list = []
+        for row in c.fetchall():
+            task_list.append({"id":row[0],"task":row[1]})
+        c.close()
+        print(task_list)
+        return render_template("map.html", task_list = task_list)
+    else:
+        return redirect("/top")
 
 
 # 新規登録でっせ
@@ -73,12 +81,8 @@ def regist_post():
     c.execute("Insert into users values (null,?,?)",(name,password))
     conn.commit()
     c.close()
-    return "登録完了"
+    return redirect("/map")
 
 
 if __name__ == "__main__":
     app.run (debug=True)
-
-
-
-    # id  name  password  seat_number  use_seat  user_id  contents  
